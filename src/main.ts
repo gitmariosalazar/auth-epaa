e8f415d3c240487198e9b5972a9da1462d1ba067
Mario Salazar <mariosalazar.ms.10@gmail.com>
2025-09-12 08:47:03 -0500

    feat(authentication): add basic project structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { environments } from './settings/environments/environments';
+import * as morgan from 'morgan';
+import { DatabaseServicePostgreSQL } from './shared/connections/database/postgresql/postgresql.service';
 
 async function bootstrap() {
+  const logger: Logger = new Logger('QRCodeMain');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(3008);
+  app.use(morgan('dev'));
+
+
+  const postgresqlService: DatabaseServicePostgreSQL = new DatabaseServicePostgreSQL();
+
+  logger.log(await postgresqlService.connect())
+  logger.log(
+    `🚀🎉 The Authentication microservice is running on: http://localhost:${3008}✅`,
+  );
+
+  const microservice = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: environments.AUTHENTICATION_KAFKA_CLIENT_ID,
+        brokers: [environments.KAFKA_BROKER_URL],
+      },
+      consumer: {
+        groupId: environments.AUTHENTICATION_KAFKA_GROUP_ID,
+        allowAutoTopicCreation: true,
+      },
+    },
+  });
+
+  await microservice.listen();
+  logger.log(`🚀🎉 The Authentication - microservice is listening to KAFKA...✅`);
 }
 bootstrap();
